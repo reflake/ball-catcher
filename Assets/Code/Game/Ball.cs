a17f01ac4a8135710be24469afa3fe4f33f2c77b
reflake <reflakeykt@gmail.com>
2024-01-03 14:10:00 +0600

    Basic ball script

---
+++ Assets/Code/Game/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+	public class Ball : MonoBehaviour
+	{
+		private void OnCollisionEnter(Collision _)
+		{
+			Remove();
+		}
+
+		// Hit the floor
+		private void Remove()
+		{
+			Destroy(gameObject);
+		}
+	}
+}
