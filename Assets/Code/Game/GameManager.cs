46c3a87d3329983a2bb957da4e1d13f2c13103fc
reflake <reflakeykt@gmail.com>
2024-01-04 15:06:20 +0600

    Exit to main menu button

---
+++ Assets/Code/Game/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+	public class GameManager : MonoBehaviour
+	{
+		public void LeaveGame()
+		{
+			SceneManager.LoadScene("MainMenuScene");
+		}
+	}
+}
