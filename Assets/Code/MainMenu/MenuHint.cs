8d532b4d42edda222554b9e32ca7f49a1b754114
reflake <reflakeykt@gmail.com>
2024-01-28 22:35:34 +0600

    visual: menu hints

---
+++ Assets/Code/MainMenu/MenuHint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace MainMenu
+{
+	public class MenuHint : MonoBehaviour
+	{
+		public static MenuHint Instance = default;
+		
+		[SerializeField] private TMP_Text label = default;
+
+		public void OnEnable()
+		{
+			Instance = this;
+		}
+
+		public void OnDisable()
+		{
+			Instance = null;
+		}
+
+		public void ShowHint(string text)
+		{
+			label.enabled = true;
+			label.text = text;
+		}
+
+		public void HideHint()
+		{
+			label.enabled = false;
+		}
+	}
+}
---
+++ Assets/Code/MainMenu/MenuHintPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace MainMenu
+{
+	public class MenuHintPoint : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+	{
+		[SerializeField, TextArea] private string hintText = string.Empty;
+		
+		public void OnPointerEnter(PointerEventData _)
+		{
+			MenuHint.Instance.ShowHint(hintText);
+		}
+
+		public void OnPointerExit(PointerEventData _)
+		{
+			MenuHint.Instance.HideHint();
+		}
+	}
+}
