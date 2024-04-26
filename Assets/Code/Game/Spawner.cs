f4a1510e3021a238840d0af71e78e96e4195cac5
reflake <reflakeykt@gmail.com>
2024-04-26 22:55:28 +0600

    feat: heart spawner

--- Assets/Code/Game/Spawner.cs
+++ Assets/Code/Game/Spawner.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Game.Collectible;
 using Game.Enum;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -9,11 +10,13 @@ namespace Game
 	{
 		[SerializeField] private GameMode targetGameMode = default;
 		[SerializeField] private Apple applePrefab = null;
+		[SerializeField] private Heart heartPrefab = null;
 		[SerializeField] private float nextSpawnTime = 0f;
 		[SerializeField] private float spawnRange = 7.2f;
 		[SerializeField] private float spawnInterval = 1.5f;
 		[SerializeField] private float spawnMinimalInterval = 0.1f;
 		[SerializeField] private float spawnIntervalIncreaseRate = 0.2f;
+		[SerializeField] private float heartSpawnChance = 0.1f;
 
 		public GameMode TargetGameMode => targetGameMode;
 
@@ -32,21 +35,47 @@ namespace Game
 			{
 				nextSpawnTime = Time.timeSinceLevelLoad + spawnInterval;
 
-				SpawnBall();
+				var item = PickNextItem(out var needRandomRotation);
+
+				SpawnItem(item, needRandomRotation);
 
 				spawnInterval -= spawnIntervalIncreaseRate / 2000;
 				spawnInterval = Mathf.Clamp(spawnInterval, spawnMinimalInterval, Single.PositiveInfinity);
 			}
 		}
 
-		private void SpawnBall()
+		private Component PickNextItem(out bool needRandomRotation)
+		{
+			if (heartPrefab != null)
+			{
+				float value = Random.value;
+
+				if (value < heartSpawnChance)
+				{
+					needRandomRotation = false;
+					
+					return heartPrefab;
+				}
+			}
+
+			needRandomRotation = true;
+			
+			return applePrefab;
+		}
+
+		private void SpawnItem(Component item, bool needRandomRotation)
 		{
 			var randomSideOffset = Vector3.right * Random.Range(-spawnRange, +spawnRange);
 			var randomPosition = transform.position + randomSideOffset;
-			var randomRotation = Random.rotationUniform;
-			var instanceOfBall = Instantiate(applePrefab, randomPosition, randomRotation);
-			
-			instanceOfBall.Setup(_scoreManager);
+			var randomRotation = needRandomRotation ? Random.rotationUniform : Quaternion.identity;
+			var instanceOfItem = Instantiate(item, randomPosition, randomRotation);
+
+			switch (instanceOfItem)
+			{
+				case Apple apple:
+					apple.Setup(_scoreManager);
+					break;
+			}
 		}
 
 		private void OnDrawGizmosSelected()
